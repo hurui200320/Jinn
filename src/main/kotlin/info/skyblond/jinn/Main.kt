d3bd5611410aa70dce7026b2f10e8b0aca27f0ac
SkyBlond <skyblond@localhost.localdomain>
2020-12-12 17:45:09 +0800

    update gradle to 6.7
    update gradle to 6.7

--- src/main/kotlin/info/skyblond/jinn/Main.kt
+++ src/main/kotlin/info/skyblond/jinn/Main.kt
@@ -3,5 +3,5 @@ package info.skyblond.jinn
 import info.skyblond.jinn.cli.Application
 
 fun main(args: Array<String>) {
-    Application.getAllSubcommands().main(args )
+    Application.getAllSubcommands().main(args)
 }
